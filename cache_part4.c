bc645ae46d0f8c0ef654639831308bd0bbc82687
meso272 <liujinyang1201@126.com>
2020-10-21 16:26:07 -0700

    dd

--- cache_part4.c
+++ cache_part4.c
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
     int i, j;
     int b;
 
-    int matrix_dim = 2046;
-    int block_size= 66;
+    int matrix_dim = 2040;
+    int block_size= 60;
     double t0, t1, elapsed_time, MKL_elapsed_time;
     double *A = (double *)malloc(sizeof(double) * matrix_dim * matrix_dim);
     double *B = (double *)malloc(sizeof(double) * matrix_dim * matrix_dim);
