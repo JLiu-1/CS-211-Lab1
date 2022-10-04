de745208ea33b58c4425227f99265f4ffb2d4d69
priyanshu-sharma <prynsh1112@gmail.com>
2022-10-04 11:13:44 -0700

    added code for dgemm0 function

--- mygemm.c
+++ mygemm.c
@@ -10,6 +10,17 @@
 //Register Reuse part 1
 void dgemm0(const double* A, const double* B, double* C, const int n)
 {
+    int i, j, k;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            for(k=0;k<n;k++)
+            {
+                C[i*n+j] += A[i*n+k] * B[k*n+j];
+            }
+        }
+    }
 
 }
 
